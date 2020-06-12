53a122747f382c5bc0be1e3fe3bf5b09f879d4a9
peter <pkaramitsos@visualantidote.com>
2020-06-12 16:08:00 -0400

    Initial Commit

---
+++ TestNuget/Calculations/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace VisualAntidote.TestNuget.Calculations
+{
+    public class Calculator
+    {
+        public static double Add(double x, double y) => x + y;
+        public static double Subtract(double x, double y) => x - y;
+    }
+}
