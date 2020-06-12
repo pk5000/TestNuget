6801f98b597b2ac72c31f749bf503808f9b1d2df
pk5000 <pkaramitsos@visualantidote.com>
2020-06-12 16:39:28 -0400

    Added a Multiply function.

--- TestNuget/Calculations/Calculator.cs
+++ TestNuget/Calculations/Calculator.cs
@@ -9,5 +9,6 @@ namespace VisualAntidote.TestNuget.Calculations
     {
         public static double Add(double x, double y) => x + y;
         public static double Subtract(double x, double y) => x - y;
+        public static double Multiply(double x, double y) => x * y;
     }
 }
